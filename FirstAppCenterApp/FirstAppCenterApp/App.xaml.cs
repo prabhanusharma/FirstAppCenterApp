c051b0c7936b75abb98e0f190aa88f54f9fcf976
Bhanu <Bhanu@DESKTOP-AQJQNFS>
2021-03-09 21:36:22 +0530

    Add project files.

---
+++ FirstAppCenterApp/FirstAppCenterApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstAppCenterApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android={Your App Secret};uwp={Your App Secret}", 
+                typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
